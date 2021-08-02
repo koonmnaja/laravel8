dfd0e8847ff32a9e99034ec1842cba0891d35d35
Rama10 <61019645+koonmnaja@users.noreply.github.com>
2021-08-02 11:29:05 +0700

    push to heroku

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 <h2>Gallery of wuttipat 62122420001</h2>
-<a href="gallery/ant1"><img src="{{$ant}}" width="300" height="177"></a>
-<img src="{{$bird}}" width="300" height="177">
+<a href="newgallery/ant"><img src="{{$ant}}" width="300" height="177"></a>
+<img src="newgallery/bird" width="300" height="177">
 <img src="{{$cat}}" width="300" height="177">
 <img src="{{$god}}" width="300" height="177">
 <img src="{{$spider}}" width="300" height="177">
--- routes/web.php
+++ routes/web.php
@@ -57,20 +57,7 @@ Route::get('/greeting', function () {
     
     return view('greeting', compact('name','last_name') );
 });
-Route::get( "/gallery" , function(){
-	$ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-    $bird = "https://www.hebergementwebs.com/image/cc/cc8811773d2cdbeb4d46e5550fc455fe.jpg/falcon-and-the-winter-soldier-falcon-minifigure-captain-america.jpg";
-    $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
-    $god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg";
-    $spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg"; 
 
-return view("test/index", compact("ant","bird","cat","god","spider") );
-});
-
-Route::get( "gallery/ant1" , function(){
-    $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-    return view("test/ant1", compact("ant") );
-});
 
 Route::get("/table", function () {
     return view("table");
@@ -83,4 +70,15 @@ Route::get("/teacher" , function (){
 Route::get("/student" , function (){
 	return view("student");
 });
+
+use App\Http\Controllers\MyProfileController;
+Route::get("/myprofile/create",[ MyProfileController::class , "create" ]);
+
+Route::get("/myprofile/{id}/edit", [ MyProfileController::class , "edit" ] );
+
+Route::get("/myprofile/{id}", [ MyProfileController::class , "show" ]);
+
+Route::get( "/newgallery" , [ MyProfileController::class , "gallery" ] );
+Route::get( "/newgallery/ant" , [ MyProfileController::class , "ant" ] );
+
 
